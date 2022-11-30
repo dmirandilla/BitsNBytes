3edd74a483792532e105afe11879a4ca070f84d7
Cameron Lau <cameron1998@gmail.com>
2022-11-30 14:03:27 -0800

    edited styles for login

--- client/src/account/LoggedIn.js
+++ client/src/account/LoggedIn.js
@@ -9,6 +9,7 @@ export default function LoggedIn() {
 
   return (
     <div className="isolate bg-white">
+      {/* Blurred Purple Top Left */}
       <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
         <svg
           className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
@@ -36,6 +37,8 @@ export default function LoggedIn() {
           </defs>
         </svg>
       </div>
+      
+      {/* LOGO TOP LEFT */}
       <div className="px-6 pt-6 lg:px-8">
         <div>
           <nav className="flex h-9 items-center justify-between" aria-label="Global">
@@ -55,46 +58,9 @@ export default function LoggedIn() {
               </button>
             </div>
           </nav>
-          <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-            <Dialog.Panel focus="true" className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
-              <div className="flex h-9 items-center justify-between">
-                <div className="flex">
-                  <a href="#" className="-m-1.5 p-1.5">
-                    <span className="sr-only">BitsNBytes</span>
-                    <img
-                      className="h-8"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                      alt=""
-                    />
-                  </a>
-                </div>
-                <div className="flex">
-                  <button
-                    type="button"
-                    className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-                    onClick={() => setMobileMenuOpen(false)}
-                  >
-                    <span className="sr-only">Close menu</span>
-                    {/* <XMarkIcon className="h-6 w-6" aria-hidden="true" /> */}
-                  </button>
-                </div>
-              </div>
-              <div className="mt-6 flow-root">
-                <div className="-my-6 divide-y divide-gray-500/10">
-                  <div className="py-6">
-                    <a
-                      href="#"
-                      className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
-                    >
-                      Log in
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </Dialog.Panel>
-          </Dialog>
         </div>
       </div>
+
       <main>
         <div className="relative px-6 lg:px-8">
           <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
@@ -128,6 +94,8 @@ export default function LoggedIn() {
                   </a>
                 </div>
               </div>
+              
+              {/* Blurred Red Right Side */}
               <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
                 <svg
                   className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
@@ -155,6 +123,7 @@ export default function LoggedIn() {
                   </defs>
                 </svg>
               </div>
+              
             </div>
           </div>
         </div>
