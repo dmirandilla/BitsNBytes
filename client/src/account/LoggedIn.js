54dea2c2dfe2df3e41574a7373f18821330bc01d
Dylan Mirandilla <dylan.mirandilla@sjsu.edu>
2022-12-02 14:15:44 -0800

    navbar

--- client/src/account/LoggedIn.js
+++ client/src/account/LoggedIn.js
@@ -32,7 +32,7 @@ const LoggedIn = () => {
 
   useEffect(() => {
     async function getNews() {
-      await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=50cb523214d247d8bbef954361ffe664&category=sports`)
+      await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=e5d03be9ccc9474d84f3813ff00ebf52&category=sports`)
         .then(res => {
           const newsArticle = res.data.articles;
           setNewsArticles(newsArticle);
@@ -45,21 +45,18 @@ const LoggedIn = () => {
         )
       }
     }
-
-    
     getNews();
     display()
-    
 
     console.log(row1)
-  }, [])
+  }, [row1])
 
 
   return (
     <div>
 
       {/* LOGO TOP LEFT */}
-  
+
 
       {/* ============================================================================================================ */}
       {/* <div class="heading flex justify-start text-gray-600 capitalize dark:text-gray-300 pt-16 pl-24">
@@ -92,86 +89,48 @@ const LoggedIn = () => {
       </div> */}
 
 
-<div className='fixed w-full h-[110px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
-      <div>
-        <img src={logoIcon} alt='Logo Image' style={{ width: '200px' }} />
-      </div>
-
-      {/* menu */}
-      <ul className='hidden md:flex'>
-        <li>
-          <Link to='home' smooth={true} duration={500}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to='about' smooth={true} duration={500}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to='skills' smooth={true} duration={500}>
-            Skills
-          </Link>
-        </li>
-        <li>
-          <Link to='work' smooth={true} duration={500}>
-            Work
-          </Link>
-        </li>
-        <li>
-          <Link to='contact' smooth={true} duration={500}>
-            Contact
-          </Link>
-        </li>
-      </ul>
-
-      {/* Hamburger */}
-      <div onClick={handleClick} className='md:hidden z-10'>
-        {!nav ? <FaBars /> : <FaTimes />}
-      </div>
-
-      {/* Mobile menu */}
-      <ul
-        className={
-          !nav
-            ? 'hidden'
-            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
-        }
-      >
-        <li className='py-6 text-4xl'>
-          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
-            Home
-          </Link>
-        </li>
-        <li className='py-6 text-4xl'>
-          {' '}
-          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
-            About
-          </Link>
-        </li>
-        <li className='py-6 text-4xl'>
-          {' '}
-          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
-            Skills
-          </Link>
-        </li>
-        <li className='py-6 text-4xl'>
-          {' '}
-          <Link onClick={handleClick} to='work' smooth={true} duration={500}>
-            Work
-          </Link>
-        </li>
-        <li className='py-6 text-4xl'>
-          {' '}
-          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
-            Contact
-          </Link>
-        </li>
-      </ul>
+      <div className='flex w-full h-[110px] px-36 justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div>
+          <img src={logoIcon} alt='Logo Image' style={{ width: '200px' }} />
         </div>
+
+        {/* menu */}
+        <ul className='flex space-x-16 hidden md:flex'>
+          <li>
+            <Link className="text-2xl border-b-2 border-transparent hover:text-red-800 dark:hover:text-red-200 hover:border-red-500" to='home' smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="text-2xl border-b-2 border-transparent hover:text-red-800 dark:hover:text-red-200 hover:border-red-500" to='about' smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link className="text-2xl border-b-2 border-transparent hover:text-red-800 dark:hover:text-red-200 hover:border-red-500" to='skills' smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link className="text-2xl border-b-2 border-transparent hover:text-red-800 dark:hover:text-red-200 hover:border-red-500" to='work' smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li>
+            <Link className="text-2xl border-b-2 border-transparent hover:text-red-800 dark:hover:text-red-200 hover:border-red-500" to='contact' smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+
+        {/* Hamburger */}
+        <div onClick={handleClick} className='md:hidden z-10'>
+          {!nav ? <FaBars /> : <FaTimes />}
+        </div>
+
+      </div>
       {/* Social icons */}
-     
+
 
 
       <div className="flex justify-center grid grid-cols-5 gap-y-40 py-40 px-40">
@@ -179,6 +138,17 @@ const LoggedIn = () => {
           <div key={o}> {o} </div>
         ))}
       </div>
+
+      <a
+        className="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+        href="/login"
+        onClick={() => logout()}
+      >
+        Logout
+        <span className="text-gray-400" aria-hidden="true">
+          &rarr;
+        </span>
+      </a>
 
 
       {/* Main */}
